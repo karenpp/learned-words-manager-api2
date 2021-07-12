8d876f7d1e1b6f5000bb0ed278a56489d02fd450
Karenpp <karenpaulapinto@gmail.com>
2021-07-11 22:58:50 -0300

    First commit

---
+++ src/main/java/one/digitalinnovation/learnedwords/LearnedwordsApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.learnedwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnedwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnedwordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/learnedwords/LearnedwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.learnedwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnedwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
